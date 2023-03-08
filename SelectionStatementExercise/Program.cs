8cd7219e8c2c7589520c2d69a1954c0ab9bf75f8
Victoria Pham <victoriapham52@gmail.com>
2023-03-08 13:00:19 -0800

    if else statement

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -2,9 +2,31 @@
 {
     public class Program
     {
+          
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            var favNumber = 52;
+            int userInput;
+           
+          
+            do 
+            {
+                Console.WriteLine("Can you guess my favorite number?");
+                userInput = int.Parse(Console.ReadLine());
+
+                if (userInput <= 51)
+                {
+                    Console.WriteLine("Too low");
+                }
+                else if (userInput >= 53)
+                {
+                    Console.WriteLine("Too high");
+                }
+                else
+                    Console.WriteLine("Spot on!");
+            }
+            while (userInput != favNumber);
         }
     }
 }
